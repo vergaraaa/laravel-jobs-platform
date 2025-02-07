14a632ac917843bf85964eb38602a2ea30cd2488
Edgar Vergara <edgarvm13@hotmail.com>
2025-02-07 15:29:50 +0100

    ✨add layout component and logo, update Tailwind CSS configuration, and import images

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+
+    @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between py-4 border-b border-white/10">
+            {{-- LOGO --}}
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Logo">
+                </a>
+            </div>
+
+            {{-- LINKS --}}
+            <div class="space-x-6 font-bold">
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+
+            {{-- POST A JOB --}}
+            <div>
+                <a href="#">Post a Job</a>
+            </div>
+        </nav>
+
+        <main class="mt-10 max-w-[986px] mx-auto">{{ $slot }}</main>
+    </div>
+</body>
+
+</html>
